d46b83cfaa143bcf11c6de3c2755ecf1696c01bb
victormarvj <vvictormarvj01@gmail.com>
2025-09-25 15:56:02 +0100

    made the date dynamic

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
 }
